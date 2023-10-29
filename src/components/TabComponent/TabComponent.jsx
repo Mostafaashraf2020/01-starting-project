e94e68d551f33d2fef9d1bada667c74a8f78e1d9
Mostafaashraf2020 <120501845+Mostafaashraf2020@users.noreply.github.com>
2023-10-29 14:47:35 +0200

    done first phase

---
+++ src/components/TabComponent/TabComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../index.css";
+
+function TabComponent({ children, onSelect, isSelected }) {
+  return (
+    <li>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
+        {children}
+      </button>
+    </li>
+  );
+}
+
+export default TabComponent;
